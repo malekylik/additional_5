9adeb157b95ac016e91d5db86b95325627209d66
malekylik <malekylik@gmail.com>
2018-02-22 15:12:56 +0300

    add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let leftBracket = [];
+  let rightBracket = [];
+
+  for(let i = 0; i < bracketsConfig.length;i++ ){
+    leftBracket.push(bracketsConfig[i][0]);
+    rightBracket.push(bracketsConfig[i][1]);
+  }
+
+  let stack = [];
+  let answer = true;
+
+
+  for(var i = 0; i < str.length;i++){
+    if(!stack.length){
+      stack.push(str[i]);
+    }else{
+      if(leftBracket.includes(str[i]) 
+        && rightBracket[leftBracket.indexOf(str[i])] !== str[i]){
+         stack.push(str[i]);
+      }else{
+        if(stack[stack.length - 1] === leftBracket[rightBracket.indexOf(str[i])]) stack.pop();
+        else stack.push(str[i]);
+      }
+    }
+  }
+
+  if(stack.length)  answer = false;
+
+    return answer;
+
 }
